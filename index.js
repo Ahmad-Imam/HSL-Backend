6e4049862f4d92bd581a9a685d88cc2adbd84a53
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2022-12-29 22:12:07 +0200

    testing gitignore fix

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use('/', indexRouter);
 const port = 8080
 
 app.use(function (req, res, next) {
+    console.log("error on app");
     next(createError(404));
 });
 app.listen(port, () => console.log(`App is running on port 8080`));
