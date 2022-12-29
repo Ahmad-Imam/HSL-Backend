6c2eecd1a3870bf2abcb99afc9ca8b7901111c80
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2022-12-29 21:48:53 +0200

    testing changing new port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({
 var indexRouter = require('./routes');
 app.use('/', indexRouter);
 
-const port = process.env.port || 8080
+const port = 8080
 
 app.use(function (req, res, next) {
     next(createError(404));
